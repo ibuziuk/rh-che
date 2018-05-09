8dceebe4c7cf4443b4513697412ee71e77bcb29e
Tibor Dancs <tdancs@redhat.com>
2018-05-09 15:32:40 +0200

    Testing RHEL build

--- .ci/cico_build_and_deploy_custom_rh-che.sh
+++ .ci/cico_build_and_deploy_custom_rh-che.sh
@@ -12,28 +12,31 @@ set +o nounset
 /usr/sbin/setenforce 0
 
 export RH_CHE_AUTOMATION_SERVER_DEPLOYMENT_URL=https://rhche-che6-automated.dev.rdu2c.fabric8.io/
-export CHE_DOCKER_BASE_IMAGE=eclipse/che-server:nightly-centos
+export BASEDIR=$(pwd)
 export ORIGIN_CLIENTS_URL=http://mirror.centos.org/centos/7/paas/x86_64/openshift-origin/origin-clients-3.7.1-2.el7.x86_64.rpm
 export OPENSHIFT_RDU2C_URL=https://dev.rdu2c.fabric8.io:8443/
 export OC_VERSION=3.9.19
+export TARGET="rhel"
+export RH_CHE_DOCKER_IMAGE_VERSION=nightly-rhcheautomation
+export DOCKER_REGISTRY="push.registry.devshift.net/rh-che-automation"
 
 function tagAndPushDocker() {
   export RH_CHE_AUTOMATION_BUILD_TAG=rh-che-automated-build-$(git rev-parse --short HEAD)
-  echo "che-docker-base-image:${CHE_DOCKER_BASE_IMAGE}"
-  docker pull ${CHE_DOCKER_BASE_IMAGE}
-  docker tag ${CHE_DOCKER_BASE_IMAGE} eclipse/che-server:local
+  source ${BASEDIR}/config
+  echo "Building for:${DOCKERFILE}"
   rm -rf ./dockerfiles/che-fabric8/eclipse-che
   mkdir ./dockerfiles/che-fabric8/eclipse-che
   cp -r ./assembly/assembly-main/target/eclipse-che-fabric8-1.0.0-SNAPSHOT/eclipse-che-fabric8-1.0.0-SNAPSHOT/* ./dockerfiles/che-fabric8/eclipse-che/
-  bash ./dockerfiles/che-fabric8/build.sh || exit 1
-  docker rmi eclipse/che-server:local
-  docker rmi ${CHE_DOCKER_BASE_IMAGE}
-  docker tag eclipse/che-server:nightly rhcheautomation/che-server:${RH_CHE_AUTOMATION_BUILD_TAG}
-  docker rmi eclipse/che-server:nightly
-  set +x
-  docker login -u ${RH_CHE_AUTOMATION_DOCKERHUB_USERNAME} -p ${RH_CHE_AUTOMATION_DOCKERHUB_PASSWORD}
-  set -x
-  docker push rhcheautomation/che-server:${RH_CHE_AUTOMATION_BUILD_TAG}
+  docker login -u "${DEFSHIFT_USERNAME}" -p "${DEVSHIFT_PASSWORD}" ${REGISTRY}
+  docker build -t ${REGISTRY}/rh-che-automation/$RH_CHE_DOCKER_IMAGE_VERSION:${RH_CHE_AUTOMATION_BUILD_TAG} || exit 1
+  docker tag ${REGISTRY}/rh-che-automation/$RH_CHE_DOCKER_IMAGE_VERSION:${RH_CHE_AUTOMATION_BUILD_TAG} ${REGISTRY}/rh-che-automation/$RH_CHE_DOCKER_IMAGE_VERSION:latest
+  docker push ${REGISTRY}/rh-che-automation/$RH_CHE_DOCKER_IMAGE_VERSION:${RH_CHE_AUTOMATION_BUILD_TAG}
+  docker push ${REGISTRY}/rh-che-automation/$RH_CHE_DOCKER_IMAGE_VERSION:latest
+  #docker tag eclipse/che-server:nightly rhcheautomation/che-server:${RH_CHE_AUTOMATION_BUILD_TAG}
+  #set +x
+  #docker login -u ${RH_CHE_AUTOMATION_DOCKERHUB_USERNAME} -p ${RH_CHE_AUTOMATION_DOCKERHUB_PASSWORD}
+  #set -x
+  ##docker push rhcheautomation/che-server:${RH_CHE_AUTOMATION_BUILD_TAG}
 }
 
 # Retrieve and test credentials
@@ -48,7 +51,7 @@ echo "***IMPORT jenkins-env NAMES DEBUG***"
 cat ./jenkins-env | sed 's;=.*;;' | sort
 echo "***==============================***"
 
-grep -E "(KEYCLOAK|BUILD_NUMBER|JOB_NAME|RH_CHE)" ./jenkins-env | sed 's/^/export /g' | sed 's/= /=/g' > ./export_env_variables
+grep -E "(DEVSHIFT|KEYCLOAK|BUILD_NUMBER|JOB_NAME|RH_CHE)" ./jenkins-env | sed 's/^/export /g' | sed 's/= /=/g' > ./export_env_variables
 if [ ! -f "./export_env_variables" ]; then
   echo "CRITICAL ERROR: sed edit of ./jeninks_env failed"
   exit 1
@@ -64,8 +67,8 @@ echo "Running ${JOB_NAME} build number #${BUILD_NUMBER}, testing creds:"
 
 CREDS_NOT_SET="false"
 curl -s "https://mirror.openshift.com/pub/openshift-v3/clients/${OC_VERSION}/linux/oc.tar.gz" | tar xvz -C /usr/local/bin
-if [ -z ${RH_CHE_AUTOMATION_DOCKERHUB_USERNAME} ] || [ -z ${RH_CHE_AUTOMATION_DOCKERHUB_PASSWORD} ]; then
-  echo "Dockerhub credentials not set"
+if [ -z ${DEVSHIFT_USERNAME} ] || [ -z ${DEVSHIFT_PASSWORD} ]; then
+  echo "Docker registry credentials not set"
   CREDS_NOT_SET="true"
 fi
 if [ -z ${RH_CHE_AUTOMATION_RDU2C_USERNAME} ] || [ -z ${RH_CHE_AUTOMATION_RDU2C_PASSWORD} ]; then
@@ -125,11 +128,11 @@ pip install yq
 # Build rh-che
 ./.ci/cico_do_build_che.sh || exit 1
 
+set +x
 # Push image to docker registry
 tagAndPushDocker
 
 # Deploy rh-che image
-set +x
 ./dev-scripts/deploy_custom_rh-che.sh -u ${RH_CHE_AUTOMATION_RDU2C_USERNAME} \
                                       -p ${RH_CHE_AUTOMATION_RDU2C_PASSWORD} \
                                       -r rhcheautomation/che-server \
